0b9c27888e291a63e43c720a71862d2946c5d51e
Fathony Mursyid <gusfathon.fm@gmail.com>
2025-12-24 20:01:00 +0700

    test: preview movies on web

---
+++ Client/movie-stream-client/src/components/movies/Movies.jsx
@@ -0,0 +1,17 @@
+import Movie from "../movie/Movie";
+
+const Movies = ({movies, message}) => {
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                {movies && movies.length > 0 
+                    ? movies.map((movie) =>(
+                        <Movie key={movie._id} movie={movie} />
+                    ))
+                    : <h2>{message}</h2>
+                }
+            </div>
+        </div>
+    )
+}
+export default Movies;
--- Server/appServer/main.go
+++ Server/appServer/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/database"
 	"github.com/GavinLonDigital/MagicStream/Server/MagicStreamServer/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -18,6 +20,17 @@ func main() {
 	})
 
 	var client *mongo.Client = database.Connect()
+
+	config := cors.Config{}
+
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PATCH"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
+	router.Use(gin.Logger())
 
 	routes.SetupUnProtectedRoutes(router, client)
 	routes.SetupProtectedRoutes(router, client)
